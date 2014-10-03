07f21fc3e64f8f8bf8071cb39007bb374b6cbdfc
croftman <trash@xsmail.fr>
2014-10-03 14:40:24 +0200

    MAJ script smileys full jquery (construction et ajout des éléments en JQ)

--- Smileys.user.js
+++ Smileys.user.js
@@ -11,55 +11,78 @@
 // @website       http://www.dvp.io
 // ==/UserScript==
  
-function getGlobal(callback) {
-	var script = document.createElement("script");
-	script.type = "text/javascript";
-	script.textContent = "(" + callback.toString() + ")();";
-	document.body.appendChild(script);
-}
+var getGlobal = function(callback) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.textContent = "(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+};
 
-function main() {
-	var anochatURI = window.location.hostname;
-	
-	$("#identAction").on("click",function() {
-		oldList = document.getElementById("apiSmileys");
-		if(oldList != null){
-			oldList.parentNode.removeChild(oldList);
-		}
-		$.get("http://api.dvp.io/smileys/",function (obj) {
-			var media = decodeURIComponent(obj.api.media);
-			var data = '<span id="apiSmileys"><fieldset id="apiSmileysTitle"><legend>'+obj.api.count+' '+obj.api.name+' v'+obj.api.version+'</legend></fieldset>';
-			$.each(obj.data.public,function(ext,list) {
-        $.each(list.split(","),function(offset,name){
-          var link = media+name+'.'+ext;
-          data += '<a href="#" onclick="$(\'#zoneSaisie\').val($(\'#zoneSaisie\').val()+\'[img]'+link+'[/img]\'); $(\'#listeSmileys\').dialog(\'close\');positionnerCurseurFin();"><img src="'+link+'" alt="'+name+'" title="'+name+'" /></a>';
-        });
-			});
-      data += '<fieldset id="apiSmileysPm" class="apiSmileysPm"><legend><img src="http://'+anochatURI+'/images/smileys/alerte.gif" alt="" /> A utiliser en MP uniquement <img src="http://'+anochatURI+'/images/smileys/alerte.gif" alt="" /></legend></fieldset>';
-      data += '<span class="apiSmileysPm" style="display:block;text-align:center;">Toute utilisation de ces smileys dans un salon public entrainera une sanction de la modération du chat</span>';
-			data += '<input type="checkbox" id="apiSmileysPmOk" /> <label for="apiSmileysPmOk" class="apiSmileysPm">J\'ai compris et je ferai attention</label>';
-      data += '<div id="apiSmileysPmList" style="display:none;">';
-			$.each(obj.data.private,function(ext,list) {
-        $.each(list.split(","),function(offset,name){
-          var link = media+'pm/'+name+'.'+ext;
-          data += '<a href="#" onclick="$(\'#zoneSaisie\').val($(\'#zoneSaisie\').val()+\'[img]'+link+'[/img]\'); $(\'#listeSmileys\').dialog(\'close\');positionnerCurseurFin();"><img src="'+link+'" alt="'+name+'" title="'+name+'" /></a>';
-        });
-			});
-			data += '</div></span>';
-			$('#listeSmileys').append(data);
-			$('#apiSmileysTitle').css({border:'none', borderTop:'1px solid #aaa', display:'block', textAlign:'center', padding:'5px',marginTop:'15px'});
-			$('#apiSmileysPm').css({border:'none', borderTop:'1px solid red', display:'block', textAlign:'center', padding:'5px'});
-			$('.apiSmileysPm').css({color:'red'});
-			$('#listeSmileys').css({overflow:'hidden', overflowY:'scroll', marginRight:'0px'});
-      $('#apiSmileysPmOk').change(function () {
-        if($('#apiSmileysPmOk').is(':checked')) {
-          $('#apiSmileysPmList').css({display:'block'});
-        } else {
-          $('#apiSmileysPmList').css({display:'none'});
+var main = function() {
+    var anochatURI = window.location.hostname;
+
+    $("#identAction").on("click",function() {
+        oldList = $("#apiSmileys");
+        if(oldList.length > 0) {
+            oldList.remove();
         }
-      });
-		});
-	});
-}
+        $.get("http://api.dvp.io/smileys/",function (obj) {
+            var media = decodeURIComponent(obj.api.media);
+            var $legendFs = $('<legend />').html(obj.api.count+' '+obj.api.name+' v'+obj.api.version);
+            var $fsSmileys = $('<fieldset />').attr('id', 'apiSmileysTitle').append($legendFs);
+            var $spanAllSmileys = $('<span />').attr('id', 'apiSmileys').append($fsSmileys);
+            
+            $.each(obj.data.public,function(ext,list) {
+                $.each(list.split(","),function(offset,name){
+                  var link = media+name+'.'+ext;
+                  var $img = $('<img />').attr({ src: link, alt: name, title: name });
+                  var $a = $('<a />').attr({ href: '#'}).on('click', function() {
+                      $('#zoneSaisie').val( $('#zoneSaisie').val() + '[img]' + link + '[/img]'); 
+                      $('#listeSmileys').dialog('close');
+                      positionnerCurseurFin();
+                  }).append($img);
+                  $spanAllSmileys.append($a);
+                });
+            });
+            
+            var $imgAlerte = $('<img />').attr({ src: 'http://'+anochatURI+'/images/smileys/alerte.gif', alt: ''});
+            var $legendPM = $('<legend />').append($imgAlerte).append('A utiliser en MP uniquement').append($imgAlerte);
+            var $fsSmileysPM = $('<fieldset />').attr('id', 'apiSmileysPm').addClass('apiSmileysPm').append($legendPM);
+            $spanAllSmileys.append($fsSmileysPM);
+            
+            var $spanWarn = $('<span />').addClass('apiSmileys').css( { display: 'block', textAlign: 'center'}).text('Toute utilisation de ces smileys dans un salon public entrainera une sanction de la modération du chat');
+            $spanAllSmileys.append($spanWarn);
+            var $labelCaution = $('<label />').attr({for: 'apiSmileysPmOk'}).addClass('apiSmileysPm').text('J\'ai compris et je ferai attention');
+            var $checkCaution = $('<input />').attr({ type: 'checkbox', id: 'apiSmileysPmOk' }).on('change', function() {
+                if($(this).prop('checked')) {
+                    $('#apiSmileysPmList').show();
+                } else {
+                    $('#apiSmileysPmList').hide();
+                }
+            });
+            $spanAllSmileys.append($checkCaution).append($labelCaution);
+            
+            var $listSmileysPrivate = $('<div />').attr('id', 'apiSmileysPmList').css('display', 'none');
+            $.each(obj.data.private,function(ext,list) {
+                $.each(list.split(","),function(offset,name){
+                    var link = media+'pm/'+name+'.'+ext;
+                    var $img = $('<img />').attr({ src: link, alt: name, title: name });
+                    var $a = $('<a />').attr({ href: '#'}).on('click', function() {
+                        $('#zoneSaisie').val( $('#zoneSaisie').val() + '[img]' + link + '[/img]'); 
+                        $('#listeSmileys').dialog('close');
+                        positionnerCurseurFin();
+                    }).append($img);
+                    $listSmileysPrivate.append($a);
+                });
+            });
+            $spanAllSmileys.append($listSmileysPrivate);
+            $('#listeSmileys').append($spanAllSmileys);
+            $('#apiSmileysTitle').css({border:'none', borderTop:'1px solid #aaa', display:'block', textAlign:'center', padding:'5px',marginTop:'15px'});
+            $('#apiSmileysPm').css({border:'none', borderTop:'1px solid red', display:'block', textAlign:'center', padding:'5px'});
+            $('.apiSmileysPm').css({color:'red'});
+            $('#listeSmileys').css({overflow:'hidden', overflowY:'scroll', marginRight:'0px'});
+        });
+    });
+};
 
 getGlobal(main);
