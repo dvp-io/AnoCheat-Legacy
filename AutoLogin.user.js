d97b6eb33c37f4b9587a92cc63a7b1d352e22041
Antoine-Pous <Antoine-Pous@users.noreply.github.com>
2014-10-04 15:00:50 +0200

    Mise à jour de la licence

--- AutoLogin.user.js
+++ AutoLogin.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name          AutoLogin - DVP I/O
-// @author				Antoine 'Gecko' Pous <gecko@dvp.io>
-// @licence       No licence, you can modify an use this script as you want
+// @author	  Antoine 'Gecko' Pous <gecko@dvp.io>
+// @licence       BEER-WARE https://github.com/Antoine-Pous/AnoCheat/blob/master/LICENSE-BEER-WARE
 // @namespace     http://www.dvp.io/fr/blog/anocheat-autologin
 // @description   Permet de se connecter automatiquement quand la page du chat est chargée.
 // @include       http://chat.developpez.com/
-// @version       2014.07.17.2
+// @version       2014.10.04.1
 // @downloadURL   http://dl.dvp.io/anocheat/AutoLogin.user.js
 // @updateURL     http://dl.dvp.io/anocheat/AutoLogin.user.js
 // @website       http://www.dvp.io
