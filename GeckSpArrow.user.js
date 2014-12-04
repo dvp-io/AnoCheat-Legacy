5fa8d04b397aab920167a4b2ae29b5b8133c575a
Antoine-Pous <Antoine-Pous@users.noreply.github.com>
2014-12-04 19:39:57 +0100

    Fix issues #13
    Ajout de l'attribut data-timer pour casser le match des messages identiques

--- GeckSpArrow.user.js
+++ GeckSpArrow.user.js
@@ -3,9 +3,9 @@
 // @author	      Antoine 'Gecko' Pous <gecko@dvp.io>
 // @licence       BEER-WARE https://github.com/Antoine-Pous/AnoCheat/blob/master/LICENSE-BEER-WARE
 // @namespace     http://dl.dvp.io/anocheat/GeckSPArrow.user.js
-// @description   Ajoute des flèches qui facilitent l'accès visuel aux interventsion sur le chat
+// @description   Ajoute des flèches qui facilitent l'accès visuel aux interventions sur le chat
 // @include       http://chat.developpez.com/
-// @version       2014.10.24.4
+// @version       2014.12.04.1
 // @downloadURL   http://dl.dvp.io/anocheat/GeckSPArrow.user.js
 // @updateURL     http://dl.dvp.io/anocheat/GeckSPArrow.user.js
 // @website       http://www.dvp.io
@@ -20,80 +20,82 @@ var getGlobal = function(callback) {
  
 var main = function() {
   
-  $(document).ajaxComplete(function(event, xhr, settings) {
-    
-    var imgInc = {
-      url:'http://media.dvp.io/anocheat/16x16_message_incoming.png',
-      rotate:0
-    };
-    var imgOut = {
-      url:'http://media.dvp.io/anocheat/16x16_message_outgoing.png',
-      rotate:0
-    };
-    var imgInf = {
-      url:'http://media.dvp.io/anocheat/16x16_message_noinfo.png',
-      rotate:0
-    };
-    
-    if(settings.url == 'ajax.php') {
+    $(document).ajaxComplete(function(event, xhr, settings) {
       
-      var data = $.parseJSON(xhr.responseText);
+      var imgInc = {
+        url:'http://media.dvp.io/anocheat/16x16_message_incoming.png',
+        rot:0
+      };
+      var imgOut = {
+        url:'http://media.dvp.io/anocheat/16x16_message_outgoing.png',
+        rot:0
+      };
+      var imgInf = {
+        url:'http://media.dvp.io/anocheat/16x16_message_noinfo.png',
+        rot:0
+      };
       
-      if(data.salon != ''){
+      if(settings.url == 'ajax.php') {
         
-        var selector = "#conversation0";
-        var entry = data.salon.split('<br />');
- 		 
-      } else if(data.pvs != ''){
-        // Pour les MP on affiche pas les flèches (comportement natif)
-        return;
-        var selector = '#conversation'.concat(data.pvs[0].id);
-        var entries = data.pvs[0].html.split('<br />');
+        var data = $.parseJSON(xhr.responseText);
         
-      } else {
+        if(data.salon != ''){
+          
+          var selector = "#conversation0";
+          var entry = data.salon.split('<br />');
+          	  		 
+        } else if(data.pvs != ''){
+          // Pour les MP on affiche pas les flèches (comportement natif)
+          return;
+          var selector = '#conversation'.concat(data.pvs[0].id);
+          var entries = data.pvs[0].html.split('<br />');
+          
+        } else {
+          
+          // Si on est pas en conv c'est une erreur on skip
+          return;
+          
+        }
         
-        // Si on est pas en conv c'est une erreur on skip
-        return;
+        /* On parcourt la liste des nouvelles entrées
+         * Fix issue #6
+         */ 
+        $.each(entry, function(e) {
+          
+          /*
+           * Si la ligne contient des images on retire le endslash des noeuds img
+           * Fix issue #2
+           */ 
+          var imgRe = entry[e].match(/(<img.*?.*?>)/gi);
+          if (imgRe instanceof Array) {
+            var i = -1;
+            while(imgRe[++i]) {
+              var newImg = imgRe[i].replace(' />','>');
+              entry[e] = entry[e].replace(imgRe[i], newImg);
+            }
+          }
+          
+          var conv = $(selector).html().toString();
+          if (entry[e].match(/images\/fleche\.png/g)) {
+            var ret = conv.replace('images/fleche.png',imgInc.url);
+            $(selector).html(ret);
+          } else {
+              var imgOutNode = '<img src="' + imgOut.url + '" title="" alt="">';
+              if ($(entry[e]).text().indexOf("[" + pseudo + "]:") === 0) {
+                /* Ajout de l'attribut data-timer pour casser le match des messages identiques
+                 * Fix issue #13
+                 */
+                var ret = conv.replace(entry[e], '<img src="'.concat(imgOut.url,'" title="" alt="">' + entry[e].replace('<span','<span data-timer="' + Date.now() + '"')));
+                $(selector).html(ret);
+              }
+          }
+          
+        });
         
       }
-      
-      /*
-       * On parcourt la liste des nouvelles entrées
-       */ 
-      $.each(entry, function(e) {
-        
-        /*
-         * Si la ligne contient des images on retire le endslash des noeuds img
-         */ 
-        var imgRe = entry[e].match(/(<img.*?.*?>)/gi);
-        if (imgRe instanceof Array) {
-          var i = -1;
-          while(imgRe[++i]) {
-            var newImg = imgRe[i].replace(' />','>');
-            entry[e] = entry[e].replace(imgRe[i], newImg);
-          }
-        }
-        
-        /*
-         * On récupère le DOM actuel
-         */ 
-        var conv = $(selector).html().toString();
-        if (entry[e].match(/images\/fleche\.png/g)) {
-          var ret = conv.replace('images/fleche.png',imgInc.url);
-          $(selector).html(ret);
-        } else {
-          if ($(entry[e]).text().indexOf("[" + pseudo + "]:") === 0) {
-            var ret = conv.replace(entry[e], '<img src="'.concat(imgOut.url,'" title="" alt="">', entry[e]));
-            $(selector).html(ret);
-          }
-        }
-        
-      });
-      
-    }
-   
-  });
-
+     
+    });
+    
 };
  
 getGlobal(main);
