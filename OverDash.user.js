517b8b73acdae860b30208960884e38ca163e2d1
Antoine-Pous <Antoine-Pous@users.noreply.github.com>
2014-12-10 11:47:31 +0100

    Add autoscroll and reduce time size

--- OverDash.user.js
+++ OverDash.user.js
@@ -5,16 +5,16 @@
 // @namespace     http://www.dvp.io/fr/blog/anocheat-overdash
 // @description   Ajoute une ligne en pointillé là où vous vous êtes arrêté de lire et où vous êtes revenus
 // @include       http://chat.developpez.com/
-// @version       2014.12.10.2
+// @version       2014.12.10.5
 // @downloadURL   http://dl.dvp.io/anocheat/OverDash.user.js
 // @updateURL     http://dl.dvp.io/anocheat/OverDash.user.js
 // @website       http://www.dvp.io
 // ==/UserScript==
 function getGlobal(callback) {
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.textContent = "(" + callback.toString() + ")();";
-    document.body.appendChild(script);
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.textContent = "(" + callback.toString() + ")();";
+  document.body.appendChild(script);
 }
 
 function overDash() {
@@ -39,16 +39,20 @@ function overDash() {
   function onchange (evt) {
     var v = "visible", h = "hidden", evtMap = {focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h};
     if (!(evt.type in evtMap))
-        if (!$("#conversation0").children().last().is("fieldset"))
-            var now = new Date();
-            var time = ('0'+now.getHours()).slice(-2)+':'+('0'+now.getMinutes()).slice(-2)+':'+('0'+now.getSeconds()).slice(-2);
-            this[hidden] ? $("#conversation0").append('<fieldset style="border-top: 1px dashed #0074cd; border-bottom: none; border-left: none; border-right: none; display: block; text-align: center; padding:0;"><legend align="center" style="padding: 0px 10px; font-size:8pt; color:#0074cd;">' + time +'</legend></fieldset>') : '';
+      if (!$("#conversation0").children().last().is("fieldset"))
+        var now = new Date();
+        var time = ('0'+now.getHours()).slice(-2)+':'+('0'+now.getMinutes()).slice(-2)+':'+('0'+now.getSeconds()).slice(-2);
+        if(this[hidden]) {
+          $("#conversation0 fieldset").remove();
+          $("#conversation0").append('<fieldset style="border-top: 1px dashed #0074cd; border-bottom: none; border-left: none; border-right: none; display: block; text-align: center; padding:0;"><legend align="center" style="padding: 0px 10px; font-size:7pt; color:#0074cd;">' + time +'</legend></fieldset>');
+        } else {
+          $("#conversation0").scrollTop($("#conversation0")[0].scrollHeight);
+        }
   }
 
   // set the initial state (but only if browser supports the Page Visibility API)
   if( document[hidden] !== undefined )
     onchange({type: document[hidden] ? "blur" : "focus"});
-    
 }
 
 getGlobal(overDash);
