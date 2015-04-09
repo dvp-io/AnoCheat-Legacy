ed0b1b4bcaa35e7335d876be72ba2174e550860d
Antoine pous <Antoine-Pous@users.noreply.github.com>
2015-04-09 12:35:50 +0200

    New features + #33 & #35
    + Gestion de la configuration via le cookie AnoCheat pour la compatibilité avec Cloud I/O et l'exportation de la configuration (#33)
    + Ajout de la prise en charge des /ALERT reçus via @ (#35)
    + Ajout d'un mode Alert sur le bouton (voir wiki)
    + Ajout d'un son spécifique aux alertes

--- Notifier.user.js
+++ Notifier.user.js
@@ -6,7 +6,7 @@
 // @namespace     http://www.dvp.io/fr/blog/anocheat-notify
 // @description   Permet d'être notifié en cas de nouveau message qui vous est destiné
 // @include       http://chat.developpez.com/
-// @version       2015.03.26.4
+// @version       2015.04.09.2
 // @downloadURL   http://dl.dvp.io/anocheat/Notify.user.js
 // @updateURL     http://dl.dvp.io/anocheat/Notify.user.js
 // @website       http://www.dvp.io
@@ -22,48 +22,94 @@ function getGlobal(callback) {
  
 function Notifier() {
 
-  var sound   = new Audio('http://media.dvp.io/anocheat/sounds/bleep.wav'),
-      arrows  = 1, // merci le "Bonjour" d'entrée :) on le compte d'avance pour éviter le *bip*
-      tabs    = 0,
-      play    = true;
+
+  var options = {
+        soundFile: 'bleep.wav',
+        alertFile: 'tardis.wav',
+        playSound: true,
+        playAlert: true,
+      },
+      arrows = 1,
+      tabs   = 0;
   
-  // Ajout du bouton à la place du lien
+  var updateCookie = function(options) {
+    var tmp_cookie = lireCookie('AnoCheat') != null ? JSON.parse(lireCookie('AnoCheat')) : {};
+    tmp_cookie.notifier = options;
+    ecrireCookie('AnoCheat', JSON.stringify(tmp_cookie), 365);
+  }
+  
+  if(jQuery('#identMemoriser').is(':checked') && lireCookie('AnoCheat') != null) {
+    var cookie = JSON.parse(lireCookie('AnoCheat'));
+    if (typeof(cookie.notifier) != "undefined") {
+      options = cookie.notifier;
+    }
+  }
+  
+  var soundNotif = new Audio('http://media.dvp.io/anocheat/sounds/' + options.soundFile),
+      soundAlarm = new Audio('http://media.dvp.io/anocheat/sounds/' + options.alertFile);
+  
+  
   $("#barreOutils").append(
     $('<input />')
       .addClass('bouton')
-      .attr({'type':'button','value':'Bip','id':'shikisound'})
+      .attr({'type':'button','id':'shikisound'})
+      .val('Bip')
       .click(function() {
-        if(play){
-          play = false;
+        
+        if(options.playSound) {
+          
+          options.playSound = false;
+          if ($('#modcp').is(':visible')) {
+            $(this).val('Alert');
+          } else {
+            $(this).css({textDecoration: 'line-through'});
+          }
+          
+        } else if (!options.playSound && $('#modcp').is(':visible') && options.playAlert) {
+          
+          options.playAlert = false;
+          $(this).val('Bip');
           $(this).css({textDecoration: 'line-through'});
-        }else{
-          play = true;
+          
+        } else {
+          
+          options.playSound = true;
+          options.playAlert = true;
           $(this).css({textDecoration: 'none'});
-          sound.play();
-        }  
+          soundNotif.play();
+          
+        }
+        
+        updateCookie(options);
       })
       .css({'background': '#729648 url(http://media.dvp.io/anocheat/01x18_button_green.png) repeat-x 0 0', 'border': '1px solid green', 'box-shadow': '0px 1px 2px #000, inset 0 .4em rgba(255,255,255,.07), inset 0 -1px 1px #459042' })
   );
   
-  function checkNotifs() {
+  function checkNotifs(is_alert) {
     
-    if (play) {
+    if (options.playAlert && is_alert) {
+      
+        soundAlarm.play();
+        
+    }
+    
+    if (options.playSound) {
       
       var tmp_tabs = $('.ongletMessage').length;
       var tmp_arrows = $('#conversation0 img[src$="fleche.png"], #conversation-1 img[src$="fleche.png"], #conversation0 img[src$="incoming.png"], #conversation-1 img[src$="incoming.png"]').length;
-      
+        
       if (tmp_arrows || tmp_tabs) {
-        
+          
         if (tmp_arrows > arrows) {
-          
-          sound.play();
+            
+          soundNotif.play();
           arrows = tmp_arrows;
-          
+            
         }
-        
-        if (tmp_tabs > tabs) {
           
-          sound.play();
+        if (tmp_tabs > tabs) {
+            
+          soundNotif.play();
           tabs = tmp_tabs;
           
         }
@@ -74,7 +120,33 @@ function Notifier() {
     
   }
 
-  $(document).ajaxComplete(function(){ checkNotifs(); });	
+  $(document).ajaxComplete(function(event, xhr, settings){
+    
+    if(settings.url == 'ajax.php') {
+      
+      var data = $.parseJSON(xhr.responseText);
+      
+      if(data.pvs.length > 0) {
+        
+        if (data.pvs.id == '-1') {
+          
+          if (data.pvs[0].html.match(/(\[Alerte aux modérateurs\])/gi)) {
+            
+            checkNotifs(true);          
+          
+          }
+        
+        }
+        
+      } else {
+      
+        checkNotifs(false);
+    
+      }
+      
+    }
+    
+  });	
 }
 
 getGlobal(Notifier);
